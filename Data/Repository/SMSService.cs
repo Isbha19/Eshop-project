9a9c11f24b5c66d0ec97f4f783574499e3226998
Isbha19 <119472126+Isbha19@users.noreply.github.com>
2024-03-13 02:39:55 +0400

    complete

---
+++ Data/Repository/SMSService.cs
@@ -0,0 +1,34 @@
+﻿using Eshop.Model.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Eshop.Data.Repository
+{
+    public class SMSService : ISMSSenderService
+    {
+        private readonly TwilioSettings twilioSettings;
+
+        public SMSService(IOptions<TwilioSettings> twilioSettings)
+        {
+            this.twilioSettings = twilioSettings.Value;
+        }
+        public async Task SendSMSAsync(string toPhone, string message)
+        {
+            TwilioClient.Init(twilioSettings.AccoundSID, twilioSettings.AuthToken);
+            await MessageResource.CreateAsync(
+                
+                to:toPhone,
+                from:twilioSettings.FromPhone,
+                body:message
+
+
+                );
+        }
+    }
+}
