bf3d0fe175ca50b8a4c996d58469fede28c633c2
Isbha19 <119472126+Isbha19@users.noreply.github.com>
2024-03-13 02:23:20 +0400

    Add files via upload

---
+++ Eshop.Model/Models/TwilioSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Model.Models
+{
+    public class TwilioSettings
+    {
+        public string? AccoundSID { get; set; }
+        public string? AuthToken { get; set; }
+        public string? FromPhone { get; set; }
+    }
+}
